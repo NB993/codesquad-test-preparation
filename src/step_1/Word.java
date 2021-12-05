a4d03b641ebbdee27a0a02d6d4f4df1eac7b93f7
jaehongChoe <qnalsfj@gmail.com>
2021-12-05 20:05:00 +0900

    [Add] Word 단어 밀어내기 기능 추가
    방향과 밀어낼 카운트를 입력받아 리스트를 자른 후 조합하여 출력

--- src/step_1/Word.java
+++ src/step_1/Word.java
@@ -1,5 +1,48 @@
 package step_1;
 
-public class Word {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
+public class Word {
+  private List<String> alphabets;
+
+  public Word(String word, String direction, int count) throws IllegalArgumentException {
+    this.alphabets = Arrays.asList(word.split(""));
+    pushAlphabet(direction, count);
+  }
+
+  //TODO : 밀어내기 구현
+  public void pushAlphabet(String direction, int count) {
+    int wordLength = alphabets.size();
+    int pushCount = count % wordLength;
+    List<String> pushedAlphabets = new ArrayList<>();
+
+    if (direction.equalsIgnoreCase("R") && count > 0) {
+      pushedAlphabets.addAll(alphabets.subList(alphabets.size() - pushCount, alphabets.size()));
+      pushedAlphabets.addAll(alphabets.subList(0, alphabets.size() - pushCount));
+      alphabets = pushedAlphabets;
+    }
+    if (direction.equalsIgnoreCase("R") && count < 0) {
+      pushedAlphabets.addAll(alphabets.subList(-pushCount, alphabets.size()));
+      pushedAlphabets.addAll(alphabets.subList(0, -pushCount));
+      alphabets = pushedAlphabets;
+    }
+    if (direction.equalsIgnoreCase("L") && count > 0) {
+      pushedAlphabets.addAll(alphabets.subList(pushCount, alphabets.size()));
+      pushedAlphabets.addAll(alphabets.subList(0, pushCount));
+      alphabets = pushedAlphabets;
+    }
+    if (direction.equalsIgnoreCase("L") && count < 0) {
+      pushedAlphabets.addAll(alphabets.subList(alphabets.size() + pushCount, alphabets.size()));
+      pushedAlphabets.addAll(alphabets.subList(0, alphabets.size() + pushCount));
+      alphabets = pushedAlphabets;
+    }
+  }
+
+  @Override
+  public String toString() {
+    return String.join("", alphabets);
+  }
 }
