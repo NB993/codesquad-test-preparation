b12f1fb736bc190e0e749cbfea30bca13fa4429e
jaehongChoe <qnalsfj@gmail.com>
2021-12-05 20:55:30 +0900

    [Update] Main.java parameter for constructor of Word
    Word클래스 생성자와 pushAlphabet메서드에서 String[] input의
    각 요소들이 어떤 값들인지를 확인할 수 있으므로 Main에서는
    배열을 통째로 넘겨주기만 하는 방향으로 변경.

--- src/step_1/Main.java
+++ src/step_1/Main.java
@@ -5,11 +5,7 @@ public class Main {
   public static void main(String[] args) {
     InputView inputView = new InputView();
     String[] input = inputView.input();
-    String inputWord = input[0];
-    String direction = input[1];
-    int count = Integer.parseInt(input[2]);
-
-    Word word = new Word(inputWord, direction, count);
+    Word word = new Word(input);
     PrintView.printWord(word);
   }
 }
