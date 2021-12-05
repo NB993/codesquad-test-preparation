38835f1ec04e1473557eb1bfbc35de2ca611d8fe
jaehongChoe <qnalsfj@gmail.com>
2021-12-05 17:40:18 +0900

    [Add] InputView 유효성 검사 추가
    1. 공백 없는 단어가 입력되었는지
    2. 밀어낼 방향을 올바르게 입력했는지
    3. 밀어낼 횟수를 올바르게 입력했는지
    4. 각 한 칸씩만 띄웠는지

--- src/step_1/InputView.java
+++ src/step_1/InputView.java
@@ -6,6 +6,52 @@ public class InputView {
   private final Scanner scanner = new Scanner(System.in);
 
   public String[] input() {
-    return scanner.nextLine().trim().split(" ");
+    String[] input = scanner.nextLine().trim().split(" ");
+    try {
+      validate(input);
+    } catch(IllegalArgumentException e) {
+      e.getMessage();
+      return input();
+    }
+    return input;
+  }
+
+  private void validate(String[] input) throws IllegalArgumentException {
+    validateLength(input);
+    validateWord(input[0]);
+    validateDirection(input[1]);
+    validateCount(input[2]);
+  }
+
+  private void validateLength(String[] input) {
+    if (input.length > 3) {
+      throw new IllegalArgumentException("공백 없는 단어, 방향, 밀어낼 횟수를 각각 한 칸씩만 띄워서 다시 입력해주세요.");
+    }
+  }
+
+  private void validateWord(String word) {
+    if (word.contains(" ")) {
+      throw new IllegalArgumentException("공백 없는 단어를 입력해주세요.");
+    }
+  }
+
+  private void validateDirection(String direction) {
+    String upperCaseDirection = direction.toUpperCase();
+    if (!upperCaseDirection.equals("L") && !upperCaseDirection.equals("R")) {
+      throw new IllegalArgumentException("방향은 l/L 또는 r/R을 입력해주세요.");
+    }
+  }
+
+  private void validateCount(String countString) {
+    int count = 0;
+    try {
+      count = Integer.parseInt(countString);
+    } catch (NumberFormatException e) {
+      throw new IllegalArgumentException("밀어낼 횟수가 제대로 입력되지 않았습니다.");
+    }
+
+    if (count < -100 || 100 < count) {
+      throw new IllegalArgumentException("밀어낼 횟수는 -100 ~ 100 사이의 값을 입력해주세요.");
+    }
   }
 }
